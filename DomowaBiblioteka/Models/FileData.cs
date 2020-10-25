d7be0ad4aaf6d6eeeb23330f3bbacd875d6ee9e2
Waldek Kamiński <neurogeniusz@gmail.com>
2020-10-25 16:46:05 +0100

    Zmiany Azurowe Waldek

---
+++ DomowaBiblioteka/Models/FileData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DomowaBiblioteka.Models
+{
+    public class FileData
+    {
+        // klasa do blobow 
+        public string FileName { get; set; }
+        public string FileSize { get; set; }
+        public string ModifiedOn { get; set; }
+    }
+}
